2b8432d6d66501291403b4fd317c0cb5d5983b72
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-11-30 00:46:25 -0800

    python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    tp = len(sys.argv) - 1
+    if tp == 0:
+        print("0 arguments.")
+    elif tp == 1:
+        print("1 argument:")
+    else:
+        print("{} argument:".format(tp))
+    for x in range(tp):
+        print("{} : {}".format(x+1, sys.argv[x + 1]))
