00e3138938cd56c5b19f4e55f31c6bf347731665
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-02 00:08:14 -0800

    python-data_structures

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    x = tuple_a[0] if len(tuple_a) > 0 else 0
+    y = tuple_a[1] if len(tuple_a) > 1 else 0
+    m = tuple_b[0] if len(tuple_b) > 0 else 0
+    n = tuple_a[1] if len(tuple_b) > 1 else 0
+    result = (x + m, y + n)
+    return result
