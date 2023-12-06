fd1051f25166ca5a4a64bcbd5c898e07d380fdbf
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-06 04:13:00 -0800

    python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    rom-num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     nv = 0
     for xv in reversed(roman_string):
-        nv = rom-num[xv]
-        res += nv if res < nv * 5 else -nv
+        nv = rom_num[xv]
+        res += nv if res < nv else -nv
     return res
