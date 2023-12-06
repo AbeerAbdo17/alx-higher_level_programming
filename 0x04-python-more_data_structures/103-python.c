9a11f577e55d7f8b5e9526955676ed3f451294ef
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-06 09:34:25 -0800

    python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,34 +1,10 @@
 #include <Python.h>
 #include <stdio.h>
+
 /**
- * print_python_list - Python lists
- * @p: input
- */
-void print_python_list(PyObject *p)
-{
-	long int xsiz, xv;
-	pyListObject *xlis;
-	PyObject *xob;
-
-	xsiz = ((pyVarObject *)(p))->ob_size;
-	xlis = (PyListObject *)p;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", xsiz);
-	printf("[*] Allocated = %ld\n", xlis->allocated);
-
-	for (xv = 0; xv < xsiz; xv++)
-	{
-		xob = ((PyListObject *)p)->ob_item[xv];
-		printf("Element %ld: %s\n", xv, ((xob)->ob_type)->tp_name);
-		if (pyBytes_check(xob))
-			print_python_bytes(xob);
-	}
-}
-/**
- * print_python_bytes - Python bytes
- * @p: input
- */
+ *  * print_python_bytes - Python bytes
+ *   * @p: input
+ *    */
 void print_python_bytes(PyObject *p)
 {
 	char *xstr;
@@ -62,3 +38,28 @@ void print_python_bytes(PyObject *p)
 	printf("\n");
 }
 
+/**
+ *  * print_python_list - Python lists
+ *   * @p: input
+ *    */
+void print_python_list(PyObject *p)
+{
+	long int xsiz, xv;
+	PyListObject *xlis;   
+	PyObject *xob;
+
+	xsiz = ((PyVarObject *)(p))->ob_size;
+	xlis = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", xsiz);
+	printf("[*] Allocated = %ld\n", xlis->allocated);
+
+	for (xv = 0; xv < xsiz; xv++)
+	{
+		xob = ((PyListObject *)p)->ob_item[xv];
+		printf("Element %ld: %s\n", xv, ((xob)->ob_type)->tp_name);
+		if (PyBytes_Check(xob))   
+			print_python_bytes(xob);
+	}
+}
