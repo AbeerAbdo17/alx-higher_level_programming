c44e036e0d9e78936c559bc3c03f1be71aba3329
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-06 05:45:05 -0800

    python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -4,36 +4,41 @@
  * print_python_list - Python lists
  * @p: input
 */
-void print_python_list(PyObject *p) {
+void print_python_list(PyObject *p)
+{
+	long int xsiz, xv;
+	pyListObject *xlis;
+	PyObject *xob;
+
+	xsiz = ((pyVarObject *)(p))->ob_size;
+	xlis = (PyListObject *)p;
+
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Size of the Python List = %ld\n", xsiz);
+	printf("[*] Allocated = %ld\n", xlis->allocated);
 
-
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-
-	for (Py_ssize_t i = 0; i < PyList_Size(p); i++) {
-		PyObject *item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	for (xv = 0; xv < xsiz; xv++)
+	{
+		xob = ((PyListObject *)p)->ob_item[xv];
+		printf("Element %ld: %s\n", xv, ((xob)->ob_type)->tp_name);
+		if (pyBytes_check(xob))
+			print_python_bytes(xob);
 	}
 }
 /**
  * print_python_bytes - Python bytes
  * @p: input
 */
-void print_python_bytes(PyObject *p) {
-	printf("[.] Bytes object info\n");
-	if (PyBytes_Check(p)) {
-		printf("  size: %ld\n", PyBytes_GET_SIZE(p));
-		printf("  trying string: %s\n", PyBytes_AS_STRING(p));
-		printf("  first 10 bytes: ");
-		for (Py_ssize_t i = 0; i < PyBytes_GET_SIZE(p) && i < 10; i++) {
-			printf("%02hhx", ((char *)PyBytes_AS_STRING(p))[i]);
-			if (i + 1 < PyBytes_GET_SIZE(p) && i + 1 < 10)
-				printf(" ");
-		}
-		printf("\n");
-	} else {
-		fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
-	}
-}
+void print_python_bytes(PyObject *p)
+{
+	char xstr;
+	llong int xsiz, xv, xlim;
 
+	printf("[.] Bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf( "  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+}
