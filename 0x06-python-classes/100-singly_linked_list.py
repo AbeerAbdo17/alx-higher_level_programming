632a759b48ca6f2e8e287a0e909ce8441952d61b
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-19 03:30:32 -0800

    100

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""define class"""
+
+
+class Node:
+    """class scop"""
+    def __init__(self, data, next_node=None):
+        """method scop"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """method scop"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """method scop"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """method scop"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """method scop"""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    """class scop"""
+    def __init__(self):
+        """method scop"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """method scop"""
+        nw = Node(value)
+        if self.__head is None or value < self.__head.data:
+            nw.next_node = self.head
+            self.__head = nw
+        else:
+            curr = self.__head
+            while curr.next_node is not None and curr.next_node.data < value:
+                curr = curr.next_node
+            nw.next_node = curr.next_node
+            curr.next_node = nw
+
+    def __str__(self):
+        """method scop"""
+        res = ""
+
+        curr = self.__head
+        while curr is not None:
+            res += str(current.data) + "\n"
+            curr = curr.next_node
+        return res
