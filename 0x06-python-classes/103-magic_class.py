1bc09979bfea36af37b93e1c4a67aa05347fbf2a
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-19 05:03:51 -0800

    103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,16 +6,16 @@ import math
 
 class MagicClass:
     """class scop"""
-    def__init__(self, redius=0):
+    def__init__(self, radius=0):
         """method scope"""
-        self.__redius = 0
-        if type(redius) is not int and type(redius) is not float:
-            raise TypeError("rediusmust be a num")
-        self.__redius = redius
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
         """method scop"""
-        return (self.__redius ** 2 * math.pi)
+        return (self.__radius ** 2 * math.pi)
 
-    def cir(self):
-        return (2 * math.pi * self.__redius)
+    def circumference(self):
+        return (2 * math.pi * self.__radius)
