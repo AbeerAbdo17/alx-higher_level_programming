bd4bad861bc008c9a5dae1fcc7262a99748bf6ae
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-01-04 03:00:29 -0800

    py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" class"""
+
+
+class Rectangle:
+    """class scop"""
+    def __init__(self, width=0, height=0):
+        """ method scop"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ method scop"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """method scop"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """method scop"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """method scop"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
