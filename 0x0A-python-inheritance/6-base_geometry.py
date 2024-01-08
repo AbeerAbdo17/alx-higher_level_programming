6aeb7c62b315045e39af49feb2e3c872c9ee0a3b
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-01-08 02:18:14 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class BaseGeometry:
+    """method scop"""
+    def area(self):
+        """method scop"""
+        raise Exception('area() is not implemented')
