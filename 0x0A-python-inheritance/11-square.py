bbd615a890b7d99d31a717fa00d9871db9d3829f
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-01-08 02:51:01 -0800

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class scop"""
+    def __init__(self, size):
+        """method scop"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """method scop"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """method scop"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
