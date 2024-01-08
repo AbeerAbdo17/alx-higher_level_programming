382f6dbaa1b21352a6a00a943207ac191fc141d5
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-01-08 01:31:26 -0800

    1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class MyList(list):
+    """class scop"""
+    def print_sorted(self):
+        """method scop"""
+        sort = sorted(self)
+        print(sort)
