1404797f63650546479ca292905c0559b83daf59
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-01-09 01:51:47 -0800

    5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """method scop"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, file)
