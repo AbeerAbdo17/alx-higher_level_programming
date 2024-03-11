c7fc4a1871b615a7840e13fa1793ced76f36de76
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-03-11 22:33:15 +0200

    mand

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	const xy = Number(process.argv[2]);
-	let xv = 0;
-	while (xv < xy) {
-		console.log('X'.repeat(xy));
-		xv++;
-	}
+  const xy = Number(process.argv[2]);
+  let xv = 0;
+  while (xv < xy) {
+    console.log('X'.repeat(xy));
+    xv++;
+  }
 }
