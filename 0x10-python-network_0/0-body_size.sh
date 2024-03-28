7abb7c82b7958de54d434ea8c7ca58b63f56b0dd
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-03-28 02:08:34 +0200

    mand

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided as an argument
-if [ $# -ne 1 ]; then
-	    echo "Usage: $0 <URL>"
-	        exit 1
-fi
-response=$(curl -Is "$1")
-
-if [ $? -ne 0 ]; then
-	    echo "Error: Unable to retrieve headers from the URL."
-	        exit 1
-fi
-
-content_length=$(echo "$response" | grep -w 'Content-Length' | cut -f2 -d' ')
-if [ -n "$content_length" ]; then
-	    echo "$content_length"
-    else
-	        echo "Content-Length header not found in the response."
-fi
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" -L | grep "Allow" | cut -d " " -f2-
