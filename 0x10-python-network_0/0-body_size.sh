59f6f6799ab16a274c6864a177f3d6468c8b9f4f
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-03-28 02:05:09 +0200

    mand

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-# script to get the body size of a request
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
+
+# Check if URL is provided as an argument
+if [ $# -ne 1 ]; then
+	    echo "Usage: $0 <URL>"
+	        exit 1
+fi
+response=$(curl -Is "$1")
+
+if [ $? -ne 0 ]; then
+	    echo "Error: Unable to retrieve headers from the URL."
+	        exit 1
+fi
+
+content_length=$(echo "$response" | grep -w 'Content-Length' | cut -f2 -d' ')
+if [ -n "$content_length" ]; then
+	    echo "$content_length"
+    else
+	        echo "Content-Length header not found in the response."
+fi
